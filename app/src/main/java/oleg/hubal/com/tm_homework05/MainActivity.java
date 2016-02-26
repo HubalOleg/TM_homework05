d49f208f05e4aff92df0b49436680e799087475c
OlegCoder <hubal.oleg@gmail.com>
2016-02-26 18:39:19 +0200

    initial commit ( project tasks: -add views, dimensions, styles, colors, strings -set listeners, read texts -send email -make call -clean code if needed )

---
+++ app/src/main/java/oleg/hubal/com/tm_homework05/MainActivity.java
@@ -0,0 +1,13 @@
+package oleg.hubal.com.tm_homework05;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
